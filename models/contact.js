3adaf74822d7f7c2cc58703ec3f058ec19928adb
Vinay Kumar <61008587+vinay25mit@users.noreply.github.com>
2022-04-12 08:17:43 +0530

    Add files via upload

---
+++ models/contact.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+const ContactSchema=new mongoose.Schema({
+      name:{
+            type:String,
+            required: true
+      },
+      phone:{
+            type:String,
+            required: true
+      }
+});
+const Contact=mongoose.model('Contact',ContactSchema);
+module.exports = Contact;
